ce85e1eeeae468a6e9b465e0d9e795a3b3380f8d
Alyona-76 <alyo-na76@yandex.ru>
2024-07-22 09:46:34 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,8 +0,0 @@
-name = 'Алена'
-print('имя',(':'), name)
-age = 47
-print('возрост',(':'), age)
-age_new = 1
-print('скоро исполнится',(':'),  age + age_new )
-is_student = True
-print( 'студент',(':'),  is_student )
